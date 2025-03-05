eea6c9bed5f0e677c1942b4e78e7d47e4a604321
Ekaterina Yurina <komsa_9191@mail.ru>
2025-03-05 16:53:22 +1000

    feat: Implement rendering functions for test page

--- src/js/renderPage.js
+++ src/js/renderPage.js
@@ -1,4 +1,6 @@
 import { questions } from './questions.js';
+import { state } from './state.js';
+import { updateAnswerSelection, handleClickButtonNext } from './eventHandlers.js';
 
 // Функция для создание HTML-элементов
 const createElement = (tag, className, parent, textContent = null) => {
@@ -11,4 +13,66 @@ const createElement = (tag, className, parent, textContent = null) => {
 
   parent.append(element);
   return element;
+};
+
+// Функция для рендеринга страницы Test
+export const renderPage = () => {
+  const body = document.body;
+  const testContainer = createElement('div', 'test__container', body);
+
+  renderHeaderTitle();
+  renderProgressBar(testContainer);
+  renderQuestion(testContainer);
+
+  const form = createElement('form', 'form', testContainer);
+
+  renderAnswers(form);
+  renderButtonNext(testContainer, form);
+
+  return testContainer;
+};
+
+// Функция для рендеринга заголовка в header
+const renderHeaderTitle = () => {
+  const headerLogoTitle = document.querySelector('.header__logo-title');
+  headerLogoTitle.classList.remove('hidden');
+  createElement('h1', 'header__title', headerLogoTitle, 'тест на определение IQ');
+};
+
+// Функция для рендеринга прогресс-бара
+const renderProgressBar = (testContainer) => {
+  const progressBar = createElement('progress', 'test__progress-bar', testContainer);
+  progressBar.id = 'progress-bar';
+  progressBar.value = 8.33;
+  progressBar.max = 100;
+};
+
+// Функция для рендеринга вопроса
+const renderQuestion = (testContainer) => {
+  createElement('h2', 'test__question', testContainer, questions[state.index].question);
+};
+
+// Функция для рендеринга вариантов ответа
+export const renderAnswers = (form) => {
+  questions[state.index].answers.forEach((answer, i) => {
+    const option = createElement('div', 'option', form);
+    const input = createElement('input', 'input', option);
+    input.type = 'radio';
+    input.name = 'answer';
+    input.id = `answer-${i}`;
+    input.value = answer;
+
+    const label = createElement('label', 'label', option, answer);
+    label.setAttribute('for', `answer-${i}`);
+
+    updateAnswerSelection(form);
+  });
+};
+
+// Функция для рендеринга кнопки Next
+const renderButtonNext = (testContainer, form) => {
+  const nextButton = createElement('button', 'button-next', testContainer, 'ДАЛЕЕ');
+  nextButton.disabled = true;
+
+  handleClickButtonNext(nextButton, form, questions);
 };
