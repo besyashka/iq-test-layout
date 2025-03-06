33cda1db5b5c0f5eada24976a21d9d9228070cc2
Ekaterina Yurina <komsa_9191@mail.ru>
2025-03-06 16:17:34 +1000

    feat: add event handling for buttons "take the test", "next", function implement update responses

---
+++ src/js/eventHandlers.js
@@ -0,0 +1,39 @@
+import { renderPage, renderAnswers, renderProcessingOfResult } from './renderPage.js';
+import { state } from './state.js';
+
+// Функция для обработки событий для кнопок 'проийти тест'
+export const handleClickButtonTest = () => {
+  document.querySelectorAll('.button-test').forEach((button) => {
+    button.addEventListener('click', () => {
+      renderPage();
+      toggleHidden(['.main', '.footer']);
+    });
+  });
+};
+
+// Обновить выбор ответа в форме
+export const updateAnswerSelection = (form) => {
+  form.addEventListener('change', (e) => {
+    state.answers[state.index] = e.target.value;
+    const buttonNext = document.querySelector('.button-next');
+    buttonNext.disabled = false;
+  });
+};
+
+// Функция для обработки событий по кнопке 'далее'
+export const handleClickButtonNext = (nextButton, form, questions) => {
+  nextButton.addEventListener('click', () => {
+    const question = document.querySelector('.test__question');
+    state.index++;
+
+    if (state.index < questions.length) {
+      question.textContent = questions[state.index].question;
+      form.innerHTML = '';
+
+      renderAnswers(form);
+    } else {
+      addHidden([question, form, nextButton]);
+      renderProcessingOfResult();
+    }
+  });
+};
