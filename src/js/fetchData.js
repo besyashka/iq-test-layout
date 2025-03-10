a91d9c07737d8f243d202e10fcec322ccf876667
Ekaterina Yurina <komsa_9191@mail.ru>
2025-03-10 13:11:43 +1000

    feat: add functions for receiving and displaying data from the API

---
+++ src/js/fetchData.js
@@ -0,0 +1,32 @@
+import { createElement } from './createElement.js';
+
+//Функция для полученных данных из API
+export const fetchData = () => {
+  const url = 'https://swapi.dev/api/people/1/';
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        return null;
+      } else {
+        return response.json();
+      }
+    })
+    .then((data) => outputReceivedData(JSON.stringify(data)));
+};
+
+// Функция для отображения полученных данных
+const outputReceivedData = (jsonData) => {
+  const divResult = document.querySelector('.result__data');
+  const data = JSON.parse(jsonData);
+  divResult.innerHTML = '';
+
+  for (let key in data) {
+    createElement('p', 'data', divResult, `${key}: ${data[key]}`);
+  }
+};
